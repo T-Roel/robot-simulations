0f4fb55302287f1e7b6831a1af18c7b25666999c
Thales Roel Pacobahyba Pessanha <thalesroel@hotmail.com>
2021-03-30 09:41:44 -0300

    desenvolvendo braço robótico

--- simulacoes/controllers/robotic_arm/robotic_arm.py
+++ simulacoes/controllers/robotic_arm/robotic_arm.py
@@ -1,32 +1,106 @@
 """robotic_arm controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
+# Import some classes of the controller module and OSC communication
 from controller import Robot
+from controller import Motor
+from controller import DistanceSensor
+from controller import PositionSensor
+from controller import TouchSensor
+from pythonosc.udp_client import SimpleUDPClient
 
-# create the Robot instance.
+def switch_case(argument):
+    switcher = {
+        "WAITING": 0,
+        "GRASPING": 1,
+        "ROTATING": 2,
+        "RELEASING": 3,
+        "ROTATING_BACK": 4
+    }
+    
+    return switcher.get(argument, "nothing")
+
+
 robot = Robot()
+counter = 0
+i = 0
+argument = "WAITING"
+target_positions = [-1.88, -2.14, -2.38, -1.51]
+speed = 1.0
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getMotor('motorname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
+# set the time step of the current world.
+timestep = 32
+ip = "127.0.0.1"
+port = 57120
+client = SimpleUDPClient(ip, port)
+
+
+# Insert a getDevice-like function in order to get the
+# instance of a device of the robot.
+hand_motors = [robot.getDevice("finger_1_joint_1"),
+               robot.getDevice("finger_2_joint_1"),
+               robot.getDevice("finger_middle_joint_1")]
+
+
+ur_motors = [robot.getDevice("shoulder_lift_joint"),
+             robot.getDevice("elbow_joint"),
+             robot.getDevice("wrist_1_joint"),
+             robot.getDevice("wrist_2_joint")]
+
+
+for i in range(4):
+    Motor.setVelocity(ur_motors[i], speed)
+    i += 1
+
+
+distance_sensor = robot.getDevice("distance sensor")
+DistanceSensor.enable(distance_sensor, timestep)
+
+
+touch_sensor = robot.getDevice("touch sensor")
+TouchSensor.enable(touch_sensor, timestep)
+
+
+position_sensor = robot.getDevice("wrist_1_joint_sensor")
+PositionSensor.enable(position_sensor, timestep)
+
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
+    if(counter <=0):
+        if(switch_case(argument) == 0):
+            if(DistanceSensor.getValue(distance_sensor) < 500):
+                counter = 8
+                argument = "GRASPING"
+                print("Gasping can\n")
+                for i in range(3):
+                    Motor.setPosition(hand_motors[i], 0.85)
+                    i += 1
+        elif(switch_case(argument) == 1):
+            for i in range(4):
+                Motor.setPosition(ur_motors[i], target_positions[i])
+                i += 1
+            print("Rotating arm\n")
+            argument = "ROTATING"
+        elif(switch_case(argument) == 2):
+            if(PositionSensor.getValue(position_sensor) < -2.3):
+                counter = 8
+                print("Releasing can \n")
+                argument = "RELEASING"
+                for i in range(3):
+                    Motor.setPosition(hand_motors[i], Motor.getMinPosition(hand_motors[i]))
+                    i += 1
+        elif(switch_case(argument) == 3):
+            for i in range(4):
+                Motor.setPosition(ur_motors[i], 0.0)
+                i += 1
+            print("Rotating arm back \n")
+            argument = "ROTATING_BACK"
+        elif(switch_case(argument) == 4):
+            if(PositionSensor.getValue(position_sensor) > -0.1):
+                argument = "WAITING"
+                print("Waiting can \n")
+    counter -= 1
+    #return 0
     pass
-
-# Enter here exit cleanup code.
